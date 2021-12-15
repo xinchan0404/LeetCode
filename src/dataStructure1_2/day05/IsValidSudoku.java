32bed6b3d2721aa9b602a887fc5e31c4d80d859f
xinchan <zhengxin2021@gmail.com>
2021-12-16 00:03:45 +0800

    leetcode-day05

--- src/dataStructure1_2/day05/IsValidSudoku.java
+++ src/dataStructure1_2/day05/IsValidSudoku.java
@@ -15,7 +15,27 @@ public class IsValidSudoku {
         /*
          * 测试 isValidSudoku()
          */
+        IsValidSudoku isValidSudoku = new IsValidSudoku();
 
+        char[][] board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        boolean isValid = false;
+
+        long startMs = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            isValid = isValidSudoku.isValidSudoku(board);
+        }
+        long costMs = System.currentTimeMillis() - startMs;
+        System.out.println("耗时：" + costMs + " ms");
+
+        System.out.println(isValid);
     }
 
     /**
@@ -31,28 +51,30 @@ public class IsValidSudoku {
 
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
+                    row[board[i][j] - '1']++;
                     if (row[board[i][j] - '1'] > 1) {
                         return false;
-                    } else {
-                        row[board[i][j] - '1']++;
                     }
                 }
 
                 if (board[j][i] != '.') {
+                    column[board[j][i] - '1']++;
                     if (column[board[j][i] - '1'] > 1) {
                         return false;
-                    } else {
-                        column[board[j][i] - '1']++;
                     }
                 }
             }
-        }
 
-        int[][][] subSudoku = new int[9][3][3];
-        int index = 0;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                subSudoku[index++][i % 3][j % 3] = board[i][j];
+            int[] subSudoku = new int[9];
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (board[j + 3 * (i / 3)][k + 3 * (i % 3)] != '.') {
+                        subSudoku[board[j + 3 * (i / 3)][k + 3 * (i % 3)] - '1']++;
+                        if (subSudoku[board[j + 3 * (i / 3)][k + 3 * (i % 3)] - '1'] > 1) {
+                            return false;
+                        }
+                    }
+                }
             }
         }
 
---
+++ src/dataStructure1_2/day05/SetZeroes.java
@@ -0,0 +1,111 @@
+package dataStructure1_2.day05;
+
+import java.util.Arrays;
+
+/**
+ * 73. 矩阵置零
+ * 提示：
+ * m == matrix.length
+ * n == matrix[0].length
+ * 1 <= m, n <= 200
+ * -231 <= matrix[i][j] <= 231 - 1
+ *
+ * @author xinchan
+ * @version 1.0.1 2021-12-15
+ */
+public class SetZeroes {
+    public static void main(String[] args) {
+        /*
+         * 测试 setZeroes()
+         */
+        SetZeroes setZeroes = new SetZeroes();
+
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+
+        setZeroes.setZeroes1(matrix);
+
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+    /**
+     * 矩阵置零 - O(m + n) 额外空间
+     *
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] row = new int[m];
+        int[] column = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    column[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || column[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * 矩阵置零 - O(1)
+     *
+     * @param matrix
+     */
+    public void setZeroes1(int[][] matrix) {
+        boolean firstRow = false;
+        boolean firstColumn = false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (matrix[0][i] == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                firstColumn = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstRow) {
+            matrix[0] = new int[n];
+        }
+        if (firstColumn) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
