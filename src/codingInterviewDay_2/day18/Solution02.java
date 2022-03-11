d6a2cb5b8a692d9f45c85aced0effe0c58f678de
xinchan <zhengxin2021@gmail.com>
2022-03-11 21:05:11 +0800

    leetcode - 0311

--- src/codingInterviewDay_2/day18/Solution02.java
+++ src/codingInterviewDay_2/day18/Solution02.java
@@ -8,11 +8,43 @@ import utils.TreeNode;
  */
 public class Solution02 {
     /**
-     * 剑指 Offer 55 - II. 平衡二叉树
+     * 剑指 Offer 55 - II. 平衡二叉树 - 自顶向下，有大量重复计算
      * @param root
      * @return
      */
     public boolean isBalanced(TreeNode root) {
-        return true;
+        if (root == null) {
+            return true;
+        }
+
+        return Math.abs(getDeep(root.left) - getDeep(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getDeep(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getDeep(root.left), getDeep(root.right)) + 1;
+    }
+
+    /**
+     * 剑指 Offer 55 - II. 平衡二叉树 - 自底向上，剪枝
+     * @param root
+     * @return
+     */
+    public boolean isBalanced1(TreeNode root) {
+        return dfs(root) >= 0;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
     }
 }
