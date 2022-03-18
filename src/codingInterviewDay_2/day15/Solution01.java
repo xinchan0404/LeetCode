a4d87385648b037800535a31b952e3171b83a29e
xinchan <zhengxin2021@gmail.com>
2022-03-19 00:00:24 +0800

    leetcode - 0318

--- src/codingInterviewDay_2/day15/Solution01.java
+++ src/codingInterviewDay_2/day15/Solution01.java
@@ -7,43 +7,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 剑指 Offer 34. 二叉树中和为某一值的路径
  * @author xinchan
  * @version 1.0.1 2022-03-08
  */
 public class Solution01 {
     private int sum;
-    private List<List<Integer>> lists = new ArrayList<>();
-    private List<Integer> list = new ArrayList<>();
-    private int cnt;
-
-    /**
-     * 剑指 Offer 34. 二叉树中和为某一值的路径
-     * @param root
-     * @param target
-     * @return
-     */
+    List<List<Integer>> lists = new ArrayList<>();
+    private List<Integer> list;
     public List<List<Integer>> pathSum(TreeNode root, int target) {
-        if (root == null) {
-            return lists;
-        }
-        pathSumRecur(root, target);
+        dfs(root, target);
         return lists;
     }
 
-    private void pathSumRecur(TreeNode root, int target) {
+    private void dfs(TreeNode root , int target) {
         if (root == null) {
             return;
         }
-        cnt++;
-        sum += root.val;
         list.add(root.val);
-        pathSumRecur(root.left, target);
-        pathSumRecur(root.right, target);
+        sum += root.val;
+        dfs(root.left, target);
+        dfs(root.right, target);
         if (root.left == null && root.right == null && sum == target) {
             lists.add(new ArrayList<>(list));
         }
-        cnt--;
         sum -= root.val;
-        list.remove(cnt);
+        list.remove(list.size() - 1);
     }
 }
