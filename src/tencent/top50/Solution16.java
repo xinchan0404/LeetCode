284ea5b6983233b286e9f83d8c296f2ac86c2735
xinchan <zhengxin2021@gmail.com>
2022-04-18 10:57:22 +0800

    lc0418(3)

--- src/tencent/top50/Solution16.java
+++ src/tencent/top50/Solution16.java
@@ -8,22 +8,10 @@ import java.util.List;
  * @version 1.0.1 2022-04-17
  */
 public class Solution16 {
-<<<<<<< HEAD
-    private int target;
-=======
->>>>>>> 47d8bc6f27f5631401af98d415ff45b883ed42b0
     private int diff = Integer.MAX_VALUE;
 
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-<<<<<<< HEAD
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right - 1) {
-            twoSum(nums, left, right);
-            if (diff == 0) break;
-            left++;
-=======
         int n = nums.length;
         for (int i = 0; i < n - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
@@ -33,7 +21,6 @@ public class Solution16 {
             if (diff == 0) {
                 break;
             }
->>>>>>> 47d8bc6f27f5631401af98d415ff45b883ed42b0
         }
         return target - diff;
     }
@@ -42,12 +29,6 @@ public class Solution16 {
         int val = nums[left++];
         while (left < right) {
             int tmp = target - (val + nums[left] + nums[right]);
-<<<<<<< HEAD
-            if (Math.abs(tmp) < Math.abs(diff)) {
-                diff = tmp;
-            }
-=======
->>>>>>> 47d8bc6f27f5631401af98d415ff45b883ed42b0
             if (tmp < 0) {
                 if (-tmp < Math.abs(diff)) {
                     diff = tmp;
