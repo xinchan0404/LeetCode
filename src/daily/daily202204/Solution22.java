f073d4036e676bc6137d18b87a15191f306f8ac6
xinchan <zhengxin2021@gmail.com>
2022-04-22 13:20:00 +0800

    lc0422

---
+++ src/daily/daily202204/Solution22.java
@@ -0,0 +1,13 @@
+package daily.daily202204;
+
+/**
+ * 396. 旋转函数
+ * @author xinchan
+ * @version 1.0.1 2022-04-22
+ */
+public class Solution22 {
+    public int maxRotateFunction(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        return 0;
+    }
+}
