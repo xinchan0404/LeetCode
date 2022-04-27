55172980190171f7dca8a5843cced59c81b39715
xinchan <zhengxin2021@gmail.com>
2022-04-27 22:59:29 +0800

    lc0427

--- src/tencent/top50/pass1/Solution122.java
+++ src/tencent/top50/pass1/Solution122.java
@@ -9,14 +9,26 @@ public class Solution122 {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int total = 0;
-        int profit = 0;
-        int buyPrice = prices[0];
         for (int i = 1; i < n; i++) {
-            if (prices[i] < buyPrice && buyPrice != 0) {
-                buyPrice = prices[i];
-            } else {
-                profit = prices[i] - buyPrice;
-            }
+            total += Math.max(0, prices[i] - prices[i - 1]);
         }
+        return total;
+    }
+}
+
+class Solution122_ {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp0 = 0;  // 第 0 天没有持有股票的收益
+        int dp1 = -prices[0];  // 第 0 天持有股票的收益
+        int dpi0;
+        int dpi1;
+        for (int i = 1; i < n; i++) {
+            dpi0 = Math.max(dp0, dp1 + prices[i]);
+            dpi1 = Math.max(dp0 - prices[i], dp1);
+            dp0 = dpi0;
+            dp1 = dpi1;
+        }
+        return Math.max(dp0, dp1);
     }
 }
--- src/tencent/top50/pass1/Solution124.java
+++ src/tencent/top50/pass1/Solution124.java
@@ -1,12 +1,30 @@
 package tencent.top50.pass1;
 
+import utils.TreeNode;
+
 /**
  * 124. 二叉树中的最大路径和
  * @author xinchan
  * @version 1.0.1 2022-04-06
  */
 public class Solution124 {
-    public void maxPathSum() {
-        return;
+    private int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    private int maxGain(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftGain = Math.max(0, maxGain(node.left));
+        int rightGain = Math.max(0, maxGain(node.right));
+
+        maxSum = Math.max(maxSum, node.val + leftGain + rightGain);
+
+        return node.val + Math.max(leftGain, rightGain);
     }
 }
