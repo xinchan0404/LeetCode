d2aef9b052c9d333c5061dc96744051a62cbb754
xinchan <zhengxin2021@gmail.com>
2022-09-16 21:05:25 +0800

    lc0429

---
+++ src/crackingInterview/pass1/ch08/Solution03.java
@@ -0,0 +1,12 @@
+package crackingInterview.pass1.ch08;
+
+/**
+ * 面试题 08.03. 魔术索引
+ * @author xinchan
+ * @version 1.0.1 2022-09-16
+ */
+public class Solution03 {
+    public int findMagicIndex(int[] nums) {
+        return 0;
+    }
+}
