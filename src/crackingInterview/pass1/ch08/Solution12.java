ef77bdf02d17ec2620ba3692b98a404ddcc82caf
xinchan <zhengxin2021@gmail.com>
2022-09-20 00:15:01 +0800

    lc - 0920

--- src/crackingInterview/pass1/ch08/Solution12.java
+++ src/crackingInterview/pass1/ch08/Solution12.java
@@ -11,63 +11,92 @@ import java.util.List;
  * @version 1.0.1 2022-09-19
  */
 public class Solution12 {
-    private int n;
-    private int[] queens;
-    private List<List<String>> ans;
-
     public List<List<String>> solveNQueens(int n) {
-        this.n = n;
-        this.queens = new int[n];
-        this.ans = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
+        int[] queens = new int[n];
+
         Arrays.fill(queens, -1);
+        dfs(ans, queens, n, 0);
 
-        dfs(0);
         return ans;
     }
 
-    private void dfs(int rows) {
+    private void dfs(List<List<String>> ans, int[] queens, int n, int rows) {
         if (rows == n) {
-            List<String> list = generateBoard();
-            ans.add(list);
+            List<String> list = generateBoard(queens, n);
+            ans.add(new ArrayList<>(list));
         }
-//        for (int i = 0; i < n; i++) {
-//            if (isValid(rows, i)) {
-//                queens[rows] = i;
-//                dfs(rows + 1);
-//                queens[rows] = -1;
-//            }
-//        }
-        boolean[] u = useless(rows);
         for (int i = 0; i < n; i++) {
-            if (!u[i]) {
+            if (isValid(queens, rows, i)) {
                 queens[rows] = i;
-                dfs(rows + 1);
+                dfs(ans, queens, n, rows + 1);
+                queens[rows] = -1;
             }
         }
     }
 
-    private boolean[] useless(int row) {
-        boolean[] ans = new boolean[n];
+    private boolean isValid(int[] queens, int row, int column) {
         for (int i = 0; i < row; i++) {
-            for (int j : new int[]{queens[i], queens[i] + row - i, queens[i] + i - row}) {
-                if (j >= 0 && j < n) {
-                    ans[j] = true;
-                }
-            }
-        }
-        return ans;
-    }
-
-    private boolean isValid(int row, int column) {
-        for (int i = 0; i < row; i++) {
-            if (queens[i] == column || queens[i] + row - i == column || queens[i] + i - row == column) {
+            if (queens[i] == column || queens[i] + row - i == column || queens[i] - row + i == column) {
                 return false;
             }
         }
         return true;
     }
 
-    private List<String> generateBoard() {
+    private List<String> generateBoard(int[] queens, int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            char[] chars = new char[n];
+            Arrays.fill(chars, '.');
+            chars[queens[i]] = 'Q';
+            list.add(new String(chars));
+        }
+        return list;
+    }
+}
+
+class Solution12_ {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        int[] queens = new int[n];
+
+        Arrays.fill(queens, -1);
+        dfs(ans, queens, n, 0);
+
+        return ans;
+    }
+
+    private void dfs(List<List<String>> ans, int[] queens, int n, int rows) {
+        if (rows == n) {
+            List<String> list = generateBoard(queens, n);
+            ans.add(new ArrayList<>(list));
+        }
+
+        boolean[] isValid = isValid(queens, n, rows);
+        for (int i = 0; i < n; i++) {
+            if (isValid[i]) {
+                queens[rows] = i;
+                dfs(ans, queens, n, rows + 1);
+                queens[rows] = -1;
+            }
+        }
+    }
+
+    private boolean[] isValid(int[] queens, int n, int row) {
+        boolean[] isValid = new boolean[n];
+        Arrays.fill(isValid, true);
+        for (int i = 0; i < row; i++) {
+            for (int j : new int[] {queens[i], queens[i] + row - i, queens[i] - row + i}) {
+                if (j >= 0 && j < n) {
+                    isValid[j] = false;
+                }
+            }
+        }
+        return isValid;
+    }
+
+    private List<String> generateBoard(int[] queens, int n) {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             char[] chars = new char[n];
