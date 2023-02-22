d6856416bcdc2518e672ebc8490084e60aa41068
xinchan <zhengxin2021@gmail.com>
2023-02-23 00:28:10 +0800

    lc - 0223

--- src/tencent/top50/pass3/Solution10.java
+++ src/tencent/top50/pass3/Solution10.java
@@ -1,5 +1,8 @@
 package tencent.top50.pass3;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 20. 有效的括号
  * @author xinchan
@@ -7,6 +10,27 @@ package tencent.top50.pass3;
  */
 public class Solution10 {
     public boolean isValid(String s) {
-        return true;
+        int n = s.length();
+        if (n % 2 != 0) {
+            return false;
+        }
+
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || (c == ')' && stack.peek() != '(')
+                        || (c == ']' && stack.peek() != '[')
+                        || (c == '}' && stack.peek() != '{')) {
+                    return false;
+                } else {
+                    stack.poll();
+                }
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
